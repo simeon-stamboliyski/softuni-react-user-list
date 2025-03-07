0a0dfdf67fcc5e7f30dc0bb7b3d6a9a742d17322
Simeon Stamboliyski <simeon062007@gmail.com>
2025-03-07 21:23:24 +0100

    fixed api async func

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,8 +3,9 @@ const baseUrl = 'http://localhost:3030/jsonstore/users'
 export default {
     async getAll() {
         const response = await fetch(baseUrl);
-        const result = response.json();
+        const result = await response.json();
+        const users = Object.values(result);
 
-        return result;
+        return users;
     }
 }
